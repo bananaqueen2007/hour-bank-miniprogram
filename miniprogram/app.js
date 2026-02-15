c370cef4ec6ee34e5f2345bdca5fb83f31a6496b
bananaqueen2007 <jiaojiao200721@stu.ouc.edu.cn>
2026-02-15 11:47:48 +0800

    初始化项目，配置云开发

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,19 +1,10 @@
 // app.js
 App({
   onLaunch: function () {
-    this.globalData = {
-      // env 参数说明：
-      // env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会请求到哪个云环境的资源
-      // 此处请填入环境 ID, 环境 ID 可在微信开发者工具右上顶部工具栏点击云开发按钮打开获取
-      env: "",
-    };
-    if (!wx.cloud) {
-      console.error("请使用 2.2.3 或以上的基础库以使用云能力");
-    } else {
-      wx.cloud.init({
-        env: this.globalData.env,
-        traceUser: true,
-      });
-    }
-  },
-});
+    // 初始化云开发环境
+    wx.cloud.init({
+      env: 'cloud1-9g7orf7l9019a6c2',  // ⚠️ 替换成你的实际云环境ID！
+      traceUser: true            // 追踪用户，便于调试
+    })
+  }
+})
